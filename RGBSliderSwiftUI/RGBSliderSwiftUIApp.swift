e26f2b83761324f329056b009581109692d36ba8
polosa00 <i@apolochanin.ru>
2023-06-12 11:00:42 +0300

    Initial Commit

---
+++ RGBSliderSwiftUI/RGBSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSliderSwiftUIApp.swift
+//  RGBSliderSwiftUI
+//
+//  Created by Александр Полочанин on 12.06.23.
+//
+
+import SwiftUI
+
+@main
+struct RGBSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
